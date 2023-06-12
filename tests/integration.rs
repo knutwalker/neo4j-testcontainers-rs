e5e72bf6ba9a4c29364f62979bb09110e3277b2a
Paul Horn <paul.horn@neotechnology.com>
2023-06-12 20:16:45 +0200

    Move all assertions into a single integration test
    This only creates one docker container, not three.

--- tests/integration.rs
+++ tests/integration.rs
@@ -6,31 +6,21 @@ use testcontainers::clients::Cli;
 async fn it_works() {
     let docker = Cli::default();
     let container = docker.run(Neo4j::default());
+
     let uri = Neo4j::bolt_uri_ipv4(&container);
+    assert!(uri.starts_with("bolt://"));
+
+    let uri = Neo4j::http_uri_ipv4(&container);
+    assert!(uri.starts_with("http://"));
+
+    let uri = Neo4j::bolt_uri_ipv4(&container);
+    let uri = uri.trim_start_matches("bolt://");
     let auth_user = container.image().user();
     let auth_pass = container.image().pass();
-
-    let uri = uri.trim_start_matches("bolt://");
 
     let graph = Graph::new(uri, auth_user, auth_pass).await.unwrap();
     let mut result = graph.execute(neo4rs::query("RETURN 1")).await.unwrap();
     let row = result.next().await.unwrap().unwrap();
     let value: i64 = row.get("1").unwrap();
     assert_eq!(1, value);
-}
-
-#[test]
-fn bolt_uri() {
-    let docker = Cli::default();
-    let container = docker.run(Neo4j::default());
-    let uri = Neo4j::bolt_uri_ipv4(&container);
-    assert!(uri.starts_with("bolt://"));
-}
-
-#[test]
-fn http_uri() {
-    let docker = Cli::default();
-    let container = docker.run(Neo4j::default());
-    let uri = Neo4j::http_uri_ipv4(&container);
-    assert!(uri.starts_with("http://"));
 }
